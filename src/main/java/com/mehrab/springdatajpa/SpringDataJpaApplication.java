728021f3e549a10de219a64e06902f246d5beea3
mehrabk <mehrab.kor@gmail.com>
2023-02-17 20:30:08 +0330

    sort & pageable demo

--- src/main/java/com/mehrab/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/mehrab/springdatajpa/SpringDataJpaApplication.java
@@ -1,11 +1,19 @@
 package com.mehrab.springdatajpa;
 
+import com.github.javafaker.Faker;
 import com.mehrab.springdatajpa.model.Student;
 import com.mehrab.springdatajpa.repository.StudentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class SpringDataJpaApplication {
@@ -16,19 +24,24 @@ public class SpringDataJpaApplication {
 
 	@Bean
 	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
-		return new CommandLineRunner() {
-			@Override
-			public void run(String... args) throws Exception {
-				studentRepository.save(new Student("mehrab", "kor", "mehrab.kor@gmail.com", 29));
-				studentRepository.save(new Student("mehrab2", "kor2", "mehrab.kor2@gmail.com", 29));
-				studentRepository.getStudentsByEmail("mehrab.kor@gmail.com").ifPresentOrElse(System.out::println, () -> System.out.println("mehrab not found"));
-				studentRepository.findStudentsByFirstNameEqualsAndAgeEquals("mehrab", 29).forEach(System.out::println);
-				studentRepository.getStudentByEmailAddress("mehrab.kor2@gmail.com").ifPresentOrElse(System.out::println, () -> System.out.println("mehrab2 not found"));
-				studentRepository.getStudentFromNativQuery("mehrab2", 20).ifPresentOrElse(System.out::println, () -> System.out.println("mehrab2 not found"));
-				System.out.println(studentRepository.deleteStudentByID(2L));
-
-			}
+		return args -> {
+			initFakeData(studentRepository);
+			Sort sort = Sort.by("age").descending();
+			PageRequest pageRequest = PageRequest.of(0, 5, sort);
+			studentRepository.findAll(pageRequest).forEach(System.out::println);
 		};
+	}
+
+	public void initFakeData(StudentRepository studentRepository) {
+		List<Student> students = new ArrayList<>();
+		Faker faker = new Faker();
+		IntStream.range(0, 20).forEach((i) -> {
+			String firstName = faker.name().firstName();
+			String lastName = faker.name().lastName();
+			String email = String.format("%s.%s@gmail.com", firstName, lastName);
+			students.add(new Student(firstName, lastName, email, faker.number().numberBetween(17, 55)));
+		});
+		studentRepository.saveAll(students);
 	}
 
 }
